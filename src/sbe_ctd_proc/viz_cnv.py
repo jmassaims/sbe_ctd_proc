bdf94c92fce21c983c8a5e81e26f9cd10f5fb68b
Arlo White <a.white@aims.gov.au>
2024-09-10 09:14:57 +1000

    cleanup cnv plot function, exclude flag,nbin by default

--- src/sbe_ctd_proc/viz_cnv.py
+++ src/sbe_ctd_proc/viz_cnv.py
@@ -14,12 +14,12 @@ from plotly import subplots
 import sbs.process.cal_coefficients as cal
 import sbs.process.conversion as conv
 import sbs.process.processing as proc
-import sbs.process.instrument_data as id
+from sbs.process.instrument_data import cnv_to_instrument_data
 import sbs.visualize.visualization as viz
 
 #print("renderers", pio.renderers)
 
-def plot(
+def sbs_plot(
     data, title, x_names, x_bounds={0: [17, 24], 1: [-1, 6], 2: [-1, 6], 3: [-1, 6]}
 ):
     config = viz.ChartConfig(
@@ -41,53 +41,102 @@ def plot(
     fig.update_layout(height=800)
     fig.show()
 
+# TODO support mode with fewer axis sharing metrics of similar range.
+def plot_for_cnv_file(cnv_file: str,
+                      axis_offset: float = 0.05,
+                      include: set[str] = None,
+                      exclude: set[str] = {'flag', 'nbin'}
+                      ) -> go.Figure:
 
-def plot_for_cnv_file(cnv_file: str):
-    cnv_data = id.cnv_to_instrument_data(cnv_file)
+    if not exclude:
+        exclude = set()
 
-    #data = pd.DataFrame(data=cnv_data, columns=["temp", "press", "cond", "flag"])
-    #cnv_data.measurements['sal00'].values
+    # always exclude pressure metrics from x axis
+    exclude.add('depSM')
+    exclude.add('prdM')
 
+    cnv_data = cnv_to_instrument_data(cnv_file)
+
+    # MeasurementSeries(label='tv290C', description='Temperature', units='ITS-90, deg C', start_time=datetime.datetime(2013, 2, 15, 8, 2, 22), values=array([29.5942, ...])
     measurements = cnv_data.measurements
     # However, MeasurementSeries already has: description, label, units, values: np.ndarray
-    vars = [(x, viz.interpret_sbs_variable(x)) for x in measurements]
+    # vars = [(x, viz.interpret_sbs_variable(x)) for x in measurements]
 
-    #plot(data=cnv_data, title="Data Conversion", x_names=["temp", "cond"], x_bounds={})
+    if 'depSM' in measurements:
+        ymetric = 'depSM'
+    else:
+        ymetric = 'prdM'
+
+    y_values = measurements[ymetric].values
+    y_var = viz.interpret_sbs_variable(ymetric)
+    #print (y_var, measurements[ymetric])
 
     fig = go.Figure()
-    fig.layout.title = "Hello"
+    # fig.layout.title = "Hello"
 
-
-    # MeasurementSeries(label='tv290C', description='Temperature', units='ITS-90, deg C', start_time=datetime.datetime(2013, 2, 15, 8, 2, 22), values=array([29.5942, 29.6049, 29.6075, 29.6117, 29.6141, 29.6124, 29.6089,
-    xmetric = "tv290C"
-    ymetric = "depSM"
-
-    # xaxis=<name of axis?>
-    # TODO maybe share axis based on units? see if any of metrics have same units or description, or interpret_sbs_variable
-    s = go.Scatter(x=measurements[xmetric].values, y=measurements[ymetric].values, name=xmetric, xaxis="x1")
-    fig.add_trace(s)
-
-    xmetric = "c0S/m"
-    s = go.Scatter(x=measurements[xmetric].values, y=measurements[ymetric].values, name=xmetric, xaxis="x2")
-    fig.add_trace(s)
-
-    #MeasurementSeries(label='c0S/m', description='Conductivity', units='S/m', start_time=datetime.datetime(2013, 2, 15, 8, 2, 22), values=array([5.656429, 5.66036 , 5.661478, 5.66785 , 5.673942, 5.676148,
-    #MeasurementSeries(label='sal00', description='Salinity, Practical', units='PSU', start_time=datetime.datetime(2013, 2, 15, 8, 2, 22), values=array([34.0755, 34.0943, 34.0997, 34.1397, 34.1791, 34.195 , 34.212 ,
-
-
-    # TODO autorange reverse
-
-    # ref apply_overlay_config
-    fig.update_layout(
-        xaxis=dict(
-            title="Foo",
-        ),
-        xaxis2=dict(
-            title="Two",
-            overlaying="x",
-            position=0.1
+    layout=dict(
+        # just need room for top-right buttons
+        margin_t=32,
+        margin_b=0,
+        hovermode='y unified',
+        yaxis=dict(
+            title=y_var['units'],
+            autorange="reversed",
+            #domain=[0.5, 1.0]
         )
-        )
+    )
+
+    if include:
+        selected_measurements = [m for m in measurements if m in include]
+    else:
+        selected_measurements = [m for m in measurements if m not in exclude]
+
+    trace_count = 0
+    for index, id in enumerate(selected_measurements):
+        m = measurements[id]
+        axis_num = index + 1
+
+        var = viz.interpret_sbs_variable(id)
+        #print(axis_num, id, var, m)
+
+        title = var['units']
+        if title == '':
+            title = m.description
+            units = m.units
+            if units:
+                title = f"{title}, {units}"
+
+        if axis_num == 1:
+            layout['xaxis'] = dict(
+                title=title,
+                showgrid=False,
+                ticks="inside",
+                ticklen=6
+            )
+        else:
+            layout[f'xaxis{axis_num}'] = dict(
+                title=title,
+                showgrid=False,
+                overlaying='x',
+                anchor='free',
+                # Unfortunately, autoshift and shift are only available on yaxis
+                # https://plotly.com/python/multiple-axes/#automatically-shifting-axes
+                # clamp to 1, otherwise error
+                position = min(axis_offset * axis_num, 1.0),
+                ticks='inside',
+                ticklen=6,
+                minor_ticks="inside"
+                #tickwidth=1
+            )
+
+        s = go.Scatter(x=m.values, y=y_values, name=id, xaxis=f"x{axis_num}")
+        fig.add_trace(s)
+        trace_count += 1
+
+    # make space for the xaxis
+    layout['yaxis']['domain'] = [axis_offset * (trace_count + 1), 1.0]
+
+    fig.update_layout(layout)
 
     return fig
 

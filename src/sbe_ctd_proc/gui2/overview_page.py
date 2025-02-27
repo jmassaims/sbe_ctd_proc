4a3872de872e2f81bc22f3245e50adbc61e01278
Arlo White <a.white@aims.gov.au>
2025-02-27 10:59:44 +1000

    improve process files button label, update on select

--- src/sbe_ctd_proc/gui2/overview_page.py
+++ src/sbe_ctd_proc/gui2/overview_page.py
@@ -1,9 +1,12 @@
+from typing import Callable
 from nicegui import ui
 
 from .dialogs import setup_file_error_dialog, setup_processing_error_dialog, setup_latitude_dialog
 from ..manager import Manager
 from ..ctd_file import CTDFile
 from .processing_state import PROC_STATE
+
+default_proc_button_label = 'Process All Raw'
 
 def select_row(*args, **kwargs):
     print(args, kwargs)
@@ -17,7 +20,7 @@ def overview_page():
 
     running_row = ui.row().classes('items-center w-full')
     with running_row:
-        ui.button('Start', icon='play_arrow', on_click=start) \
+        proc_button = ui.button(default_proc_button_label, icon='play_arrow', on_click=start) \
             .bind_visibility_from(PROC_STATE, 'is_processing', value=False)
         ui.linear_progress(show_value=False).style('max-width: 400px;') \
             .bind_value_from(PROC_STATE, 'progress') \
@@ -74,6 +77,34 @@ def overview_page():
 
     mgr.on_change = refresh
 
+    def update_proc_button(selection):
+        distinct_status = set(s['status'] for s in selection)
+        one_status = len(distinct_status) == 1
+        # TODO change status values soon. variables names chosen for future status
+        is_raw = 'pending' in distinct_status
+        # TODO processing/partial (future status values)
+        is_processing = 'processed' in distinct_status
+        is_done = 'done' in distinct_status
+
+        if one_status and is_processing:
+            label = 'Reprocess Selected'
+        elif one_status and is_raw:
+            label = 'Process Selected'
+        elif is_raw and is_processing:
+            label = '[Re]Process Selected'
+        elif is_done:
+            # TODO review with Jack. Currently, file processing code skips done.
+            # probably better to show message that selected Approved files are skipped.
+            label = 'Reprocess Approved does nothing!'
+        else:
+            # button  action should default to process raw.
+            label = default_proc_button_label
+
+        proc_button.set_text(label)
+
+
+    table.on_select = update_proc_button
+
 
 def ctdfile_to_row(ctdfile: CTDFile):
     ctdfile.parse_hex()
@@ -91,8 +122,12 @@ def ctdfile_to_row(ctdfile: CTDFile):
 class CTDFilesTable:
     filter_status: str | None = None
 
+    # called when selection changes. given the selected objects created for table.
+    on_select: Callable | None
+
     def __init__(self, mgr: Manager):
         self.mgr = mgr
+        self.on_select = None
 
         columns = [
             {'name': 'base_file_name', 'label': 'Base Name', 'field': 'base_file_name', 'sortable': True },
@@ -108,7 +143,9 @@ class CTDFilesTable:
         rows = [ctdfile_to_row(row) for row in mgr.ctdfiles]
 
         # TODO autosort cast_date desc
-        table = ui.table(columns=columns, rows=rows, row_key='base_file_name', selection='multiple')
+        table = ui.table(columns=columns, rows=rows, row_key='base_file_name', selection='multiple',
+                         on_select=lambda : self.__on_select())
+
         self.table = table
 
         table.add_slot('body-cell-status', '''
@@ -156,9 +193,17 @@ class CTDFilesTable:
             filtered_rows = [ctdfile_to_row(row) for row in self.mgr.ctdfiles if f(row.status())]
             self.table.update_rows(filtered_rows)
 
+        # above resets selection, but does not trigger on_select
+        self.__on_select()
+
     def refresh(self):
         self.filter(self.filter_status)
 
     def get_selected(self):
         """get selected base file names"""
         return [row['base_file_name'] for row in self.table.selected]
+
+    def __on_select(self):
+        """table selection changed"""
+        if self.on_select:
+            self.on_select(self.table.selected)

0e93fffb60c42b133960648988a4825ce06b615e
Arlo White <a.white@aims.gov.au>
2025-04-10 13:24:49 +1000

    refactor ctd file page tabs into separate files

---
+++ src/sbe_ctd_proc/gui2/components/__init__.py
@@ -0,0 +1,3 @@
+from .cnv_chart import PlotSection
+from .data_checker import build_negative_cols_view
+from .scan_counts import build_scan_counts_view
--- src/sbe_ctd_proc/gui2/ctd_file_page.py
+++ src/sbe_ctd_proc/gui2/ctd_file_page.py
@@ -1,128 +1,15 @@
 import logging
 import os
-from pathlib import Path
 from typing import Optional
 
 from nicegui import ui
 
-from seabirdscientific.instrument_data import cnv_to_instrument_data, \
-    InstrumentData, MeasurementSeries
-
-from ..analysis import create_scan_count_dataframe, check_for_negatives
+from ..analysis import check_for_negatives
 from .processing_state import PROC_STATE
 from ..config import CONFIG
 from ..ctd_file import CTDFile
-from ..viz_cnv import plot_for_cnv_file
+from .components import PlotSection, build_negative_cols_view, build_scan_counts_view
 from .widgets import error_message
-
-class PlotSection:
-    """CNV file selection and Measurement selection with chart"""
-
-    selected_cnv: str
-
-    def __init__(self, ctdfile: CTDFile, cnv_paths: list[Path]) -> None:
-        cnv_names = [x.name for x in cnv_paths]
-        selected_cnv = cnv_names[-1]
-        self.cnv_dir = cnv_paths[0].parent
-        steps_done, total_steps = ctdfile.get_step_count()
-
-        with ui.row(align_items='center'):
-            ui.select(cnv_names, value=selected_cnv,
-                          on_change=lambda e: self.show_cnv(e.value)
-                          ).bind_value(self, 'selected_cnv')
-
-            if steps_done == total_steps:
-                ui.chip(f'{steps_done} steps', color='gray', text_color='white')
-            else:
-                ui.chip(f'{steps_done}/{total_steps} steps', color='orange', text_color='white')
-
-            ui.button("Select Measurements",
-                      on_click=lambda: self.open_measurements_dialog())
-
-        self.plot_container = ui.column().classes('w-full').style('flex: auto')
-
-        self.measurements_dialog = MeasurementsDialog()
-
-        self.show_cnv(selected_cnv)
-
-    def show_cnv(self, filename: str, include: set[str] | None = None):
-        """Re-create the plot for the given cnv file.
-         @param filename: CNV filename in the cnv_dir
-         @param include: measurements to display
-        """
-
-        # completely recreate the plot, could consider plotly update in future.
-        self.plot_container.clear()
-
-        cnv_path = self.cnv_dir / filename
-        instrument_data = cnv_to_instrument_data(cnv_path)
-
-        self.measurements_dialog.update(instrument_data)
-
-        if include is None:
-            selected = self.measurements_dialog.get_selected()
-            if len(selected) == 0:
-                include = {'tv290C'}
-
-        fig = plot_for_cnv_file(instr_data=instrument_data, include=include)
-
-        with self.plot_container:
-            ui.plotly(fig).classes('w-full h-full')
-
-    async def open_measurements_dialog(self):
-        result = await self.measurements_dialog.dialog
-
-        # always update for now. could add dialog buttons
-        selected = self.measurements_dialog.get_selected()
-        self.show_cnv(self.selected_cnv, set(selected))
-
-
-class MeasurementsDialog:
-    """Dialog for multi-selecting measurements"""
-
-    # measurement labels to omit
-    ignored = {'depSM', 'prdM'}
-
-    def __init__(self) -> None:
-        columns = [
-            {'name': 'id', 'label': 'Label', 'field': 'id'},
-            {'name': 'desc', 'label': 'Description', 'field': 'desc'},
-            {'name': 'units', 'label': 'Units', 'field': 'units'}
-        ]
-
-        with ui.dialog() as dialog, ui.card() as card:
-            # remove default max-width
-            card.style('max-width: unset')
-            self.dialog = dialog
-            self.table = ui.table(columns=columns, rows=[], row_key='id', selection='multiple')
-
-    def update(self, instrument_data: InstrumentData):
-        rows = self.build_rows(instrument_data)
-        self.table.update_rows(rows, clear_selection=False)
-
-        if not self.table.selected:
-            # default selection using description
-            default_select = {'Temperature', 'Conductivity'}
-            selected = [r for r in self.table.rows if r['desc'] in default_select]
-            self.table.selected = selected
-
-    def open(self):
-        self.dialog.open()
-
-    def build_rows(self, instrument_data: InstrumentData):
-        def measurement_to_row(m):
-            return {
-                'id': m.label,
-                'desc': m.description,
-                'units': m.units
-            }
-
-        return [measurement_to_row(m)
-                for m in instrument_data.measurements.values()
-                if m.label not in self.ignored]
-
-    def get_selected(self) -> list[str]:
-        return [row['id'] for row in self.table.selected]
 
 
 @ui.page('/ctd_file/{base_file_name}')
@@ -282,11 +169,8 @@ def ctd_file_page(base_file_name: str):
                 PlotSection(ctdfile, cnv_files)
 
         if derive_file:
-            df = create_scan_count_dataframe(derive_file)
-
             with ui.tab_panel(sc_tab):
-                ui.label(f'Scanned: {derive_file}')
-                ui.table.from_pandas(df)
+                build_scan_counts_view(derive_file)
 
         if bin_file:
             with ui.tab_panel(neg_tab):
@@ -316,17 +200,3 @@ def get_prev_next_files(current_name: str) -> tuple[Optional[CTDFile], Optional[
     prev_file = ctdfiles[index - 1] if index > 0 else None
     next_file = ctdfiles[index + 1] if index < len(ctdfiles) - 1 else None
     return prev_file, next_file
-
-def build_negative_cols_view(file: Path, negative_cols: list[MeasurementSeries]):
-    ui.label(f'Scanned: {file}')
-    ui.label(f'Negative values found in {len(negative_cols)} columns.')
-    if negative_cols:
-        # ui.label(f'Negative values found in columns: {negative_cols}')
-        for col in negative_cols:
-            ui.label(col.label).classes('text-h5')
-            ui.label(f'{col.description} ({col.units})')
-            neg_values = list(float(x) for x in col.values if float(x) < 0)
-            ui.label(f'Negative values: {neg_values}')
-
-    else:
-        ui.label('No negative values found in any column.')
